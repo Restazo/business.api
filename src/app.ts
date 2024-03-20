78aaf0c5e8eeb08a06fd8767978d43e5f2c4d416
Matheus Oliveira <t2fema00@students.oamk.fi>
2024-03-20 14:33:14 +0200

    API: Typescript migration

--- src/app.ts
+++ src/app.ts
@@ -21,6 +21,8 @@ app.use(
 
 app.use(express.json())
 
+
+
 app.use("/test", testRoutes)
 
 app.listen(process.env.API_PORT, () => {
--- src/db.ts
+++ src/db.ts
@@ -1,18 +1,20 @@
 import pg from "pg"
 
+
+console.log(process.env.DB_HOST)
 const pool = new pg.Pool({
   database: process.env.DB_DATABASE,
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PWD,
-  port: process.env.DB_PORT,
+  port: 3000,
 })
 
 pool.connect((err, client, release) => {
   if (err) {
-    console.error("Error connection to database", err.stack)    
+    console.error("Error connection to database", err.stack)
   }
-  console.log("Database connection succesful")
+  console.log("Database connection succesful..")
   release()
 })
 
