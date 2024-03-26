e1ce247b5d35070f43f731cbefee14a86e5cbf2e
Matheus Oliveira <t2fema00@students.oamk.fi>
2024-03-26 12:45:23 +0200

    enhance: Helper functions for JWT(sign, verify, update on db, delete on db). Helper functions to get business(user) by email or id

---
+++ src/data/business.ts
@@ -0,0 +1,43 @@
+import { pool } from "../db/db.js";
+
+import { Business } from "../schemas/types.js";
+
+export const getBusinessById = async (id: string) => {
+  try {
+    const existingBusiness = await pool.query(
+      "SELECT * FROM business WHERE id = $1",
+      [id]
+    );
+
+    if (existingBusiness.rows.length < 0) {
+      return null;
+    }
+
+    const business: Business = existingBusiness.rows[0];
+
+    return business;
+  } catch (error) {
+    console.log(error);
+    return null;
+  }
+};
+
+export const getBusinessByEmail = async (email: string) => {
+  try {
+    const existingBusiness = await pool.query(
+      "SELECT * FROM business WHERE email = $1",
+      [email]
+    );
+
+    if (existingBusiness.rows.length <= 0) {
+      return null;
+    }
+
+    const business: Business = existingBusiness.rows[0];
+
+    return business;
+  } catch (error) {
+    console.log(error);
+    return null;
+  }
+};
