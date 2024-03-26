84d377b7e8e5778b69574f55b60548abab94dc50
Matheus Oliveira <t2fema00@students.oamk.fi>
2024-03-26 11:26:38 +0200

    typedef: Extend ENV to include JWT secret/expiry variable. Extend JWT payload type, LoginSchema defined

--- src/schemas/schemas.ts
+++ src/schemas/schemas.ts
@@ -16,9 +16,23 @@ export const RegisterSchema = z.object({
   email: z.string().email(),
   password: z.string().min(8),
   confirm_password: z.string().min(8),
-  business_name: z.string(),
+  business_name: z.string().min(1),
   card: z.string().length(16), // this will probably change
   address: AddressSchema,
+});
+
+export const LoginSchema = z.object({
+  email: z.string().email(),
+  password: z.string().min(1),
+});
+
+export const BusinessSchema = z.object({
+  id: z.string().min(1),
+  email: z.string().email(),
+  name: z.string().min(1),
+  password: z.string().min(1),
+  card: z.string().length(16),
+  refresh_token: z.string().optional(),
 });
 
 export const EnvSchema = z.object({
@@ -28,5 +42,9 @@ export const EnvSchema = z.object({
   DB_PWD: z.string().min(1),
   DB_HOST: z.string().min(1),
   DB_PORT: z.string().min(1),
+  ACCESS_TOKEN_SECRET: z.string().min(1),
+  REFRESH_TOKEN_SECRET: z.string().min(1),
+  ACCESS_TOKEN_EXPIRY: z.string().min(1),
+  REFRESH_TOKEN_EXPIRY: z.string().min(1),
   GOOGLE_ADDRESS_VALIDATION_URL: z.string().min(1),
 });
--- src/schemas/types.ts
+++ src/schemas/types.ts
@@ -1,10 +1,23 @@
 import * as z from "zod";
+import { JwtPayload } from "jsonwebtoken";
 
-import { AddressSchema, ExtendedAddressSchema } from "./schemas.js";
+import {
+  AddressSchema,
+  ExtendedAddressSchema,
+  BusinessSchema,
+} from "./schemas.js";
+
+export type JwtCustomPayload = JwtPayload & {
+  id: string;
+  email: string;
+  name: string;
+};
 
 export type Address = z.infer<typeof AddressSchema>;
 
 export type ExtendedAddress = z.infer<typeof ExtendedAddressSchema>;
+
+export type Business = z.infer<typeof BusinessSchema>;
 
 export type AddressValidationResult = {
   data?: ExtendedAddress;
