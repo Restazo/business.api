f6d82ed9c2b410924ea1e57e6f221a1a5400b37a
Matheus Oliveira <t2fema00@students.oamk.fi>
2024-03-26 21:23:13 +0200

    refactor: Serialize middleware to have stricter checks

--- src/middleware/deserializeUser.ts
+++ src/middleware/deserializeUser.ts
@@ -1,4 +1,5 @@
 import { NextFunction, Request, Response } from "express";
+import jwt from "jsonwebtoken";
 
 import { verifyAccessToken, verifyRefreshToken } from "../lib/jwt-utils.js";
 import { getBusinessById } from "../data/business.js";
@@ -13,17 +14,50 @@ const deserializeUser = async (
 
     const { refreshToken } = req.cookies;
 
-    // If no access token
-    if (!accessToken) {
+    // If no access token or refresh token
+    if (!accessToken || !refreshToken) {
+      return next();
+    }
+
+    const accessTokenPayload: any = jwt.decode(accessToken);
+    const refreshTokenPayload: any = jwt.decode(refreshToken);
+
+    // If access or refresh token doesnt have payload Or property id
+    if (
+      !accessTokenPayload ||
+      !refreshTokenPayload ||
+      !("id" in accessTokenPayload) ||
+      !("id" in refreshTokenPayload)
+    ) {
+      return next();
+    }
+
+    // If tokens ID does not match
+    if (accessTokenPayload.id !== refreshTokenPayload.id) {
       return next();
     }
 
     const decodedAccessToken = verifyAccessToken(accessToken);
+    const decodedRefreshToken = verifyRefreshToken(refreshToken);
+
+    // For valid access token
+    if (decodedAccessToken.payload) {
+      try {
+        const user = await getBusinessById(decodedAccessToken.payload.id);
+        req.user = user;
+        return next();
+      } catch (error) {
+        console.log(error);
+        return next();
+      }
+    }
+
+    if (!decodedAccessToken && !refreshToken) {
+      return next();
+    }
 
     // For expired access token, but refresh token is present
     if (!decodedAccessToken.payload && refreshToken) {
-      const decodedRefreshToken = verifyRefreshToken(refreshToken);
-
       // If not valid refresh token
       if (!decodedRefreshToken.payload) {
         console.log("refresh token is null");
@@ -36,21 +70,9 @@ const deserializeUser = async (
 
         // If refresh token doesnt match our DB token
         if (user?.refresh_token !== refreshToken) {
+          console.log("refresh token doesnt match");
           return next();
         }
-
-        req.user = user;
-        return next();
-      } catch (error) {
-        console.log(error);
-        return next();
-      }
-    }
-
-    // For valid access token
-    if (decodedAccessToken.payload) {
-      try {
-        const user = await getBusinessById(decodedAccessToken.payload.id);
         req.user = user;
         return next();
       } catch (error) {
